f8f27ff345d9c80465c9c245d636bc94064c907f
hacker <hacker@st10.os3.site>
2017-11-14 18:32:24 +0300

    Added data merging

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ import struct
 from PIL import Image
 import os
 
+
 def encode_in_pixel(byte, pixel):
     """Encodes a byte in the two least significant bits of each channel.
     A 4-channel pixel is needed, which should be a tuple of 4 values from 0 to
@@ -22,7 +23,7 @@ def encode_in_pixel(byte, pixel):
 
 
 class Header:
-    MAX_FORMAT_LENGTH=8
+    MAX_FORMAT_LENGTH = 8
     magicnum = "hide"
     size = 0
     fformat = "txt"
@@ -41,19 +42,18 @@ def decode_from_pixel(pixel):
     return struct.pack("B", result)
 
 
-def encode(image, data, filename, encryption=False, password=""):
+def encode(image, data, filename):
     im = Image.open(image)
     px = im.load()
 
     # Create a header
     header = Header()
     header.size = len(data)
-    header.fformat = "" if (len(filename.split(os.extsep))<2)\
-                     else filename.split(os.extsep)[1]
+    header.fformat = "" if (len(filename.split(os.extsep)) < 2) else filename.split(os.extsep)[1]
 
     # Add the header to the file data
     header_data = struct.pack("4s" +
                              "I" +
-                             str(Header.MAX_FORMAT_LENGTH)+"s",\
+                             str(Header.MAX_FORMAT_LENGTH)+"s",
                              header.magicnum, header.size, header.fformat)
     filebytes = header_data + data
