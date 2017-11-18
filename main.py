8acf250c444c9b027d903be9c7d3c61f7eda9d06
hacker <hacker@st10.os3.site>
2017-11-19 00:59:33 +0300

    main

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import image
 import join
 import imgen
+import math
 import split
 import random
 
-width = random.randrange(500, 1000)
-height = random.randrange(500, 1000)
+width = 1000
+height = 1000
 
-imgen.get_image(1000, 1000)
+f = open("Test.mp3", "rb")
+data = f.read()
+
+# Suppose that we have 1000x1000px image
+imgen.get_image(height, width)
+
+n_pics = int(math.ceil(len(data) / float(width*height)))
+
+pics = []
+for i in range(1, n_pics):
+    pics.append(imgen.get_image(height, width))
+
+
