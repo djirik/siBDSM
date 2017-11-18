b43bca69e558ce0690bbcb36b7c83babaf0820ed
hacker <hacker@st10.os3.site>
2017-11-18 22:53:04 +0300

    fractal edit

--- fractal.py
+++ fractal.py
@@ -8,28 +8,29 @@ def julia(n, m, itermax, xmin, xmax, ymin, ymax, name):
     xmin, xmax, ymin, ymax specify the region of the
     set to compute.
     """
-    ix, iy = mgrid[0:n, 0:m] # The points of 2D arrays giving the x-coord and y-coord at each point in the array
+    ix, iy = mgrid[0:n, 0:m]  # The points of 2D arrays giving the x-coord and y-coord at each point in the array
     x = linspace(xmin, xmax, n)[ix]  #linspace(start, end, n) 
     y = linspace(ymin, ymax, m)[iy]
-    z = x + complex(0,1)*y # z is the complex number with the given x, y coords
+    z = x + complex(0, 1) * y  # z is the complex number with the given x, y coords
     del x, y 
     # c = random(1)*1.2 - .5 + complex(0,1)*(random(1)*1.-.5)
     r = 0.8 + random(1) / 5
     phi = random(1)*2*pi
-    c = r*cos(phi) + complex(0,1)*r*sin(phi)
+    c = r*cos(phi) + complex(0, 1) * r * sin(phi)
     img = zeros(z.shape, dtype=int)
     ix.shape = n*m
     iy.shape = n*m
     z.shape = n*m
     PARS = random(3) / 2
     for i in range(itermax):
-        if not len(z): break 
-        if random(1) < PARS[0] :
+        if not len(z):
+            break
+        if random(1) < PARS[0]:
             z = log(z)
-        if random(1) < PARS[1] :
+        if random(1) < PARS[1]:
             multiply(z, z, z)
-        if random(1) < 0.5 :
-            z = 1 / (complex(1,1)-z)
+        if random(1) < 0.5:
+            z = 1 / (complex(1, 1)-z)
         else :
             multiply(z, z, z)
         if random(1) < PARS[2] :
@@ -37,7 +38,7 @@ def julia(n, m, itermax, xmin, xmax, ymin, ymax, name):
         else :
             add(z, -c, z)
             
-        rem = abs(z)>2.0
+        rem = abs(z) > 2.0
         img[ix[rem], iy[rem]] = i+1
         rem = ~rem
         z = z[rem]
